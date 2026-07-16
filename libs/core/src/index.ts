3e729e5c1eabfc3928f94dbad27129bcbf992887
Bobby Bol <studio.innua@gmail.com>
2026-07-16 13:19:32 +0200

    Export supabase from barrel

--- libs/core/src/index.ts
+++ libs/core/src/index.ts
@@ -4,5 +4,11 @@ export { getConfig, loadConfig, setConfig } from './config/index.js';
 export type { LoadConfigOptions, NxtConfig } from './config/index.js';
 export { requireEnv } from './config/require-env.js';
 
-export { demoModules } from './modules/demo/demo-modules.js';
+export { SUPABASE_QUERY_LIMIT } from './constants.js';
+export {
+  GlobalSupabaseModule,
+  SupabaseService,
+  throwSupabaseError,
+} from './modules/supabase/supabase.module.js';
 
+export { demoModules } from './modules/demo/demo-modules.js';
